c3a141b4cdee775addf1a4c493f03b1ac7b1d10b
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-09-11 01:20:09 +0200

    tests, sessionID checking

--- tests/test_user_config.py
+++ tests/test_user_config.py
@@ -1,11 +1,15 @@
-import tempfile
-import os
-import yaml
-import pytest
 import argparse
-from leetcode.configuration import UserConfig  # Replace 'your_module' with the actual module where UserConfig is defined
+import os
+import tempfile
 
-# Define a fixture to create a temporary configuration file
+import pytest
+import yaml
+
+from leetcode.configuration import \
+    UserConfig 
+
+
+# fixture for temporary config file
 @pytest.fixture
 def temp_config_file():
     temp_dir = tempfile.mkdtemp()
@@ -24,35 +28,34 @@ def temp_config_file():
 
     return temp_file_path  # Provide the temporary file path to the tests
 
+# fixture for UserConfig object with temporary config file
 @pytest.fixture
-def user_config_with_temp_file(temp_config_file):
+def user_config_with_temp_file(temp_config_file: str):
     return UserConfig(config_path=temp_config_file)
 
 # Test UserConfig initialization
-def test_userconfig_init(temp_config_file):
+def test_userconfig_init(temp_config_file: str):
     user_config = UserConfig(config_path=temp_config_file)
     assert user_config.path == temp_config_file
     assert user_config.data is not None
 
 # Test UserConfig get method
-def test_userconfig_get(temp_config_file):
+def test_userconfig_get(temp_config_file: str):
     user_config = UserConfig(config_path=temp_config_file)
     assert user_config.get('csrv_token') == 'example_token'
 
 # Test UserConfig dump_key method
-def test_userconfig_dump_key(temp_config_file):
+def test_userconfig_dump_key(temp_config_file: str):
     user_config = UserConfig(config_path=temp_config_file)
     
-    # Dump a key-value pair and check if it's updated in the config file
     user_config.dump_key('new_key', 'new_value')
     
-    # Read the config file to check if the value is updated
     with open(temp_config_file, 'r') as yaml_file:
         data = yaml.safe_load(yaml_file)
     assert data['user_data']['new_key'] == 'new_value'
 
 # Test UserConfig execute method with valid args
-def test_userconfig_execute_valid_args(user_config_with_temp_file, capsys):
+def test_userconfig_execute_valid_args(user_config_with_temp_file: UserConfig, capsys: pytest.CaptureFixture[str]):
     args = argparse.Namespace(config_key='csrv_token', config_value='new_token')
     user_config_with_temp_file.execute(args)
 
@@ -62,10 +65,12 @@ def test_userconfig_execute_valid_args(user_config_with_temp_file, capsys):
     assert "Configuration updated successfully." in captured.out
 
 # Test UserConfig execute method with invalid config_key
-def test_userconfig_execute_invalid_key(user_config_with_temp_file, capsys):
+def test_userconfig_execute_invalid_key(user_config_with_temp_file: UserConfig, capsys: pytest.CaptureFixture[str]):
     args = argparse.Namespace(config_key='invalid_key', config_value='new_value')
     user_config_with_temp_file.execute(args)
 
     # Check if an error message is printed for an invalid key
     captured = capsys.readouterr()
     assert "Invalid key: invalid_key" in captured.out
+    
+# argparse module handles the case when either of the arguments is missing
