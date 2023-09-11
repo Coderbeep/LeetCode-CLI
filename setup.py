9c4cb5e71c42b52f756c85cf011d4d3b86acc337
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-09-11 12:59:21 +0200

    entry_points for CLI and package_data

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='Leetcode API Client',
       version='0.1',
-      packages=find_packages())
+      packages=find_packages(),
+      entry_points={'console_scripts': ['leet = leetcode.main:main'],},
+      package_data={'': ['*.yaml', '*.graphql']},
+      )
