9a896e08c956ff4f5f4c1d72485b05c3f719ade5
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-09-12 14:04:43 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='Leetcode API Client',
-      version='0.1',
+setup(name='leetcode_cli',
+      version='0.2',
+      description='A CLI tool to access LeetCode',
+      author='Jakub Kubiak',
       packages=find_packages(),
       entry_points={'console_scripts': ['leet = leetcode.main:main'],},
       package_data={'': ['*.yaml', '*.graphql']},
