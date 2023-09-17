f2c2260dbb5a95a8978729f09a01ebfb09b92370
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-09-17 13:51:48 +0200

    submission prep

--- leetcode/models/test_model_submission.py
+++ leetcode/models/test_model_submission.py
@@ -1,44 +1,45 @@
 from leetcode.models import *
 import requests
+import argparse
+# TODO: Add a decorator to check if the user is logged in
+# TODO: getting code from file
+# TODO: getting data input
+from graphql_get_question_detail import getQuestionDetail
 
 class sendSubmission(QueryTemplate):
     def __init__(self):
         super().__init__()
-        self.graphql_query = None
-        self.result = None  
-        self.params = {"lang": "python3",
-                       "question_id": "88",
-                       "typed_code": "class Solution:\n    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:\n        \"\"\"\n        Do not return anything, modify nums1 in-place instead.\n        \"\"\"\n        ",
-                       "data_input": "[1,2,3,0,0,0]\n3\n[2,5,6]\n3\n[1]\n1\n[]\n0\n[0]\n0\n[1]\n1"
-                        }
-    
         self.submit_url = "https://leetcode.com/problems/merge-sorted-array/submit/"
         self.runcode = ''
     
-    def execute(self):
-        response = requests.post(url="https://leetcode.com/problems/merge-sorted-array/interpret_solution/",
-                                    headers=self.config.headers,
-                                    json=self.params,
-                                    cookies=self.config.cookies)
-        self.runcode = response.json()['interpret_id']
+    def typed_code(self, filename):
+        with open(filename, 'r') as file:
+            code = file.read()
+        return code
             
-    def execute_check(self):
-        response = requests.get(url=f"https://leetcode.com/submissions/detail/{self.runcode}/check/",
+    def execute_check(self, title_slug, filename):
+        question = getQuestionDetail(title_slug)
+        self.params = {"lang": "python3",
+                       "question_id": question.question_id,
+                       "typed_code": self.typed_code(filename),
+                       "data_input": question.sample_test_case}
+        
+        # Interpret solution
+        response = requests.post(url=f"https://leetcode.com/problems/{title_slug}/interpret_solution/",
+                                 headers=self.config.headers,
+                                 json=self.params,
+                                 cookies=self.config.cookies)
+        runcode = response.json()['interpret_id']
+        
+        response = requests.get(url=f"https://leetcode.com/submissions/detail/{runcode}/check/",
                             headers=self.config.headers,
                             cookies=self.config.cookies)
     
-        while response.json().get('state') == 'PENDING' or response.json().get('state') == 'STARTED':
-            response = requests.get(url=f"https://leetcode.com/submissions/detail/{self.runcode}/check/",
+        while response.json().get('state') == 'STARTED' or response.json().get('state') == 'PENDING':
+            response = requests.get(url=f"https://leetcode.com/submissions/detail/{runcode}/check/",
                                     headers=self.config.headers,
                                     cookies=self.config.cookies)
-    
         print(response.json())
 
-subm = sendSubmission()
-subm.execute()
-
-subm.execute_check()
-
-# submission_details = submissionDetails(123)
-# submission_details.execute()
-
+submission = sendSubmission()
+submission.execute_check('add-two-integers', 'good_solution.py')
