fcf6f83d3f2a1b10a41c46d46b1b2ace99fa02de
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-09-17 13:50:40 +0200

    changes for submission preparation

--- leetcode/models/graphql_get_question_detail.py
+++ leetcode/models/graphql_get_question_detail.py
@@ -1,20 +1,45 @@
 from leetcode.models import *
 
 class getQuestionDetail(QueryTemplate):
-    def __init__(self):
+    def __init__(self, title_slug: str):
         super().__init__()
-        # Instance specific variables
-        self.question_slug = ""
-        
+        # Instance-specific variables
+        self._title_slug = title_slug
+
         self.graphql_query = None
         self.result = None
-        self.params = {'titleSlug': 'two-sum'}
-        
+        self.params = {'titleSlug': title_slug}
+
+        # Initialize variables to store query data
+        self._question_data = None
+        self.execute()
+
     def execute(self):
         self.graphql_query = GraphQLQuery(self.query, self.params)
         self.result = self.leet_API.post_query(self.graphql_query)
-        print(self.result)
-        
+        # Store the entire question data
+        self._question_data = self.result.get('data', {}).get('question', {})
 
-instance = getQuestionDetail()
-instance.execute()
+    @property
+    def title_slug(self):
+        return self._title_slug
+
+    @property
+    def question_id(self):
+        return self._question_data.get('questionId')
+
+    @property
+    def question_frontend_id(self):
+        return self._question_data.get('questionFrontendId')
+
+    @property
+    def title(self):
+        return self._question_data.get('title')
+
+    @property
+    def content(self):
+        return self._question_data.get('content')
+    
+    @property
+    def sample_test_case(self):
+        return self._question_data.get('sampleTestCase')
--- leetcode/models/problem_by_id_slug.py
+++ leetcode/models/problem_by_id_slug.py
@@ -1,20 +1,40 @@
-from . import *
+from leetcode.models import *
+from leetcode.configuration import Configuration
+from leetcode.leet_api import LeetAPI
 
-class problemByIDSlug(QueryTemplate):
+class problemInfo():
+    API_URL = "https://leetcode.com/api/problems/all/"
+    configuration = Configuration()
+    leet_api = LeetAPI(configuration)
+    
     def __init__(self):
-        super().__init__()
-        self.api_url = "https://leetcode.com/api/problems/all/"
         self.title_slug: str = None
 
+    @classmethod
+    def get_title_slug(cls, question_id: int) -> str:
+        response = cls.leet_api.get_request(cls.API_URL)
+        for item in response.get('stat_status_pairs', []):
+            if item['stat'].get('question_id') == question_id:
+                return item['stat'].get('question__title_slug', '')
+        return None
+    
+    @classmethod
+    def get_id(cls, title_slug: str) -> int:
+        response = cls.leet_api.get_request(cls.API_URL)
+        for item in response.get('stat_status_pairs', []):
+            if item['stat'].get('question__title_slug') == title_slug:
+                return item['stat'].get('question_id', 0)
+        return None
+
     def execute(self, args):
-        RESULT = self.leet_API.get_request(self.api_url)
+        result = self.leet_api.get_request(self.API_URL)
         if getattr(args, 'id'):
-            for x in RESULT['stat_status_pairs']:
-                if x['stat']['question_id'] == args.id:
-                    self.title_slug = x['stat']['question__title_slug']
+            for item in result.get('stat_status_pairs', []):
+                if item['stat'].get('question_id') == args.id:
+                    self.title_slug = item['stat'].get('question__title_slug', '')
                     break
-            if self.title_slug is None:
-                console.print("Invalid ID have been provided. Please try again.", style=ALERT)
+            if not self.title_slug:
+                console.print("Invalid ID has been provided. Please try again.", style=ALERT)
                 sys.exit(1)
         elif getattr(args, 'slug'):
             self.title_slug = args.slug
