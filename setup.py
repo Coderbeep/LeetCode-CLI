c6a39ee7168796a03c362a588a98333185c67290
Coderbeep <77514091+Coderbeep@users.noreply.github.com>
2023-10-04 18:16:54 +0200

    version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open ("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='pyleetcode-cli',
-      version='0.1.0',
+      version='0.2.0',
       description='A CLI tool to access LeetCode',
       long_description=long_description,
       long_description_content_type="text/markdown",
